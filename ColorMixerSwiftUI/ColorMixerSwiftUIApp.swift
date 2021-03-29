9aa3d1162ac916ecb3a71e96bac03716bd52ff1d
Maria Mamina <maria.mamina3@gmail.com>
2021-03-29 09:48:55 +0300

    Initial Commit

---
+++ ColorMixerSwiftUI/ColorMixerSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorMixerSwiftUIApp.swift
+//  ColorMixerSwiftUI
+//
+//  Created by Maria Mamina on 29.03.2021.
+//
+
+import SwiftUI
+
+@main
+struct ColorMixerSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
